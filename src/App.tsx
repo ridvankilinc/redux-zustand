d9ddb83306b4d020993b3613d8df1e9754eb9932
Rıdvan <ridvankilinc762@gmail.com>
2024-08-02 11:53:06 +0300

    feat: redux api added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import { Counter } from "./redux/features/counter/Counter";
+import { useGetPokemonByNameQuery } from "./redux/services/pokemon";
 import { CounterZustand } from "./zustand/Counter";
 
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  console.log(data);
+
   return (
     <div className="flex h-screen justify-center items-center bg-gray-300">
       <div className=" flex flex-1 justify-center items-center ">
         <Counter />
+      </div>
+      <div>
+        {error ? (
+          <>Oh no, there was an error</>
+        ) : isLoading ? (
+          <>Loading...</>
+        ) : data ? (
+          <div className="flex flex-col gap-4">
+            <h3 className="text-center">{data.species.name}</h3>
+            <img src={data.sprites.front_shiny} alt={data.species.name} />
+          </div>
+        ) : null}
       </div>
       <div className="flex flex-1 justify-center items-center">
         <CounterZustand />
--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import { pokemonApi } from "../services/pokemon";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
